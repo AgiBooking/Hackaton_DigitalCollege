4ef96d91800589c2d2344fe6e601c58452c2cfce
DeboraMuro <debora.flaviabm@hotmail.com>
2023-05-24 00:15:08 -0300

    component criado

--- src/pages/Eventos/index.jsx
+++ src/pages/Eventos/index.jsx
@@ -1,9 +1,10 @@
 import './index.css';
+import CriarEvento from '../../components/CriarEvento';
 
 export default function Eventos() {
     return (
         <>
-            Eventos
+            <CriarEvento/>
         </>
     )
 }
