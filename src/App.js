d3f53eb36655a936c71aec58a3f0ff4b5b834274
DeboraMuro <debora.flaviabm@hotmail.com>
2023-05-23 22:25:30 -0300

    rotas e pastas finalizadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import PermanentDrawerLeft from './components/Drawer';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Eventos from './pages/Eventos';
+import Salas from './pages/Salas';
+import Usuarios from './pages/Usuarios';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
         <PermanentDrawerLeft/>
+        <Routes>
+          <Route path='/eventos' element={<Eventos/>} />
+          <Route path="/salas" element={<Salas/>}/>
+          <Route path="/usuarios" element={<Usuarios/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
