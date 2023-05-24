879b2fbd7a8ab568cadb7d29de6ed54ebe3c97e8
Rosana Marques <rosanadeveloper@gmail.com>
2023-05-24 18:45:15 -0300

    Modifica estilo do componente Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import './App.css';
 import Login from './pages/Login'
 
+import './App.css';
+import ResponsiveDrawer from './components/Drawer';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Eventos from './pages/Eventos';
+import Salas from './pages/Salas';
+import Usuarios from './pages/Usuarios';
+import Login from './pages/Login';
+
+const authRoutes = [
+  {
+    path: '/eventos',
+    element: <Eventos />
+  },
+  {
+    path: '/salas',
+    element: <Salas />
+  },
+  {
+    path: '/usuarios',
+    element: <Usuarios />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  }
+
+]
+
+const ResponsivePage = ({element}) => (
+  <>
+    <ResponsiveDrawer />
+    {element}
+  </>
+)
+
+
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          {
+            authRoutes.map((route, index) =>
+              <Route key={index} path={route.path} element={<ResponsivePage element={route.element}/>} />
+            )
+          }
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,8 +7,6 @@ import FormularioLogin from '../../components/FormularioLogin';
 export default function Login() {
   return (
     <>
-      {/* <Banner/> */}
-      {/* <ColecoesDestaqueIcones /> */}
       <div className="background-login">
         <FormularioLogin />
       </div>
