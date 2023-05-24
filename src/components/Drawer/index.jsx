0b368efe3ef87aebafb3d77318f0a655e8cacc82
DeboraMuro <debora.flaviabm@hotmail.com>
2023-05-24 08:50:16 -0300

    melhorando responsividade do Drawer

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,93 +1,156 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
+import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PersonIcon from '@mui/icons-material/Person';
+import { Link } from '@mui/material';
 import './index.css';
 import logo from '../../img/logo colorida.jpeg'
-import { Link } from '@mui/material';
 
 const drawerWidth = 240;
 
 const Bottoes_navegacao = [
-    {
-        icon: <DashboardIcon/>,
-        name: 'Dashboard',
-        path: './dashboard'
-    },
-    {
-        icon: <MeetingRoomIcon/>,
-        name: 'Salas',
-        path: './salas'
-    },
-    {
-        icon: <CalendarMonthIcon/>,
-        name: 'Eventos',
-        path: './eventos'
-    },    
-    {
-        icon: <PersonIcon/>,
-        name: 'Usuários',
-        path: './usuarios'
-    }    
-
+  {
+      icon: <DashboardIcon/>,
+      name: 'Dashboard',
+      path: './dashboard'
+  },
+  {
+      icon: <MeetingRoomIcon/>,
+      name: 'Salas',
+      path: './salas'
+  },
+  {
+      icon: <CalendarMonthIcon/>,
+      name: 'Eventos',
+      path: './eventos'
+  },    
+  {
+      icon: <PersonIcon/>,
+      name: 'Usuários',
+      path: './usuarios'
+  }    
 ];
 
-export default function PermanentDrawerLeft() {
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar className='logo'>
+        <img src={logo} alt='logo da Agii Booking' />
+      </Toolbar>
+      <Divider />
+      <List>
+        {Bottoes_navegacao.map((item, index) => (
+          <Link href={item.path} underline="none" color="inherit">
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>      
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.name} />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+          </Link>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar
         position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
-      </AppBar>
-      <Drawer
         sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
         }}
-        variant="permanent"
-        anchor="left"
       >
-        <Toolbar className='logo'>
-            <img src={logo} alt='logo da Agii Booking' />
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* Infos da navbar */}
         </Toolbar>
-        <Divider />
-        <List>
-            {Bottoes_navegacao.map((item, index) => (
-            <Link href={item.path} underline="none" color="inherit">
-                <ListItem key={index} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>      
-                        {item.icon}
-                        </ListItemIcon>
-                        <ListItemText primary={item.name} />
-                    </ListItemButton>
-                </ListItem>
-            </Link>
-          ))}
-        </List>
-      </Drawer>
-        <Box
-            component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-            <Toolbar />
-        </Box>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+      </Box>
     </Box>
   );
 }
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
